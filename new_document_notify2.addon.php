03580cfc16494d32f1268375c9fe2918a09eab51
canto87-mac <canto87@naver.com>
2013-11-26 23:41:08 +0900

    #4 If Act is getFileList and procFileUpload, Won't delete Session of Notify.

--- new_document_notify2.addon.php
+++ new_document_notify2.addon.php
@@ -5,54 +5,63 @@
 	*/
 if(!defined('__XE__')) exit();
 
-if($called_position == 'before_module_proc' && Context::get('module') != 'admin'){
+if($called_position == 'before_module_proc' && Context::get('module') != 'admin' && Context::get('act') != 'getFileList' && Context::get('act')!='procFileUpload'){
 	Context::addJsFile('./addons/new_document_notify2/js/socket.io.js');
 	Context::loadFile(array("./addons/new_document_notify2/js/new_document_notify.js", 'body', '', -100000), true);
 		
 	Context::addCssFile('./addons/new_document_notify2/css/style.css');
 	Context::addBodyHeader('<div id="notify-div"></div>');
 	Context::addHtmlHeader('<script type="text/javascript">var socket = io.connect("'.$addon_info->nitrous.'");var delay = '.$addon_info->delay.' </script>');
-	if($_SESSION['notify_type']=='write'){
-		$title = $_SESSION['notify_title'];
-		$srl =  $_SESSION['notify_srl'];
-		unset($_SESSION['notify_type']);
-		unset($_SESSION['notify_title']);
-		unset($_SESSION['notify_srl']);
-		$new_document_notify = sprintf("<script type='text/javascript'>
-				var socket = io.connect('".$addon_info->nitrous."');
-				socket.emit('sendToServer', {title :'".$title."' , document_srl : '".$srl."'});
-				</script>");
-		Context::addHtmlHeader($new_document_notify);
-	}else if($_SESSION['notify_type']=="comment"){
-		$title = $_SESSION['notify_title'];
-		$srl = $_SESSION['notify_srl'];
-		$name = $_SESSION['notify_name'];
-		$type = $_SESSION['notify_type'];
-		unset($_SESSION['notify_type']);
-		unset($_SESSION['notify_title']);
-		unset($_SESSION['notify_srl']);
-		unset($_SESSION['notify_name']);
-		$new_document_notify = sprintf("<script type='text/javascript'>
-				var socket = io.connect('".$addon_info->nitrous."');
-				socket.emit('sendToServer', {title :'".$title."' , document_srl : '".$srl."' , name : '".$name."' , type : '".$type."'});
-				</script>");
-		Context::addHtmlHeader($new_document_notify);
+
+	if(Context::get('act') == "dispBoardWrite" && Context::get('document_srl')){
+		$_SESSION['notify_modify'] = 'modify';
 	}else{
-		if($_SESSION['notify_type']=='comment') unset($_SESSION['notify_name']);
-		unset($_SESSION['notify_type']);
-		unset($_SESSION['notify_title']);
-		unset($_SESSION['notify_srl']);
-	}
+		if(Context::get('act') == 'procBoardInsertDocument' && $_SESSION['notify_modify'] == "modify"){
+			 
+		}else{
+			if($_SESSION['notify_type']=='write'){
+				
+				$title = $_SESSION['notify_title'];
+				$srl =  $_SESSION['notify_srl'];
+				unset($_SESSION['notify_type']);
+				unset($_SESSION['notify_title']);
+				unset($_SESSION['notify_srl']);
+				$new_document_notify = "<script type='text/javascript'>
+											socket.emit('sendToServer', {title :'".$title."' , document_srl : '".$srl."'});
+										</script>";
+				Context::addHtmlHeader($new_document_notify);
+			}else if($_SESSION['notify_type']=="comment"){
+				
+				$title = $_SESSION['notify_title'];
+				$srl = $_SESSION['notify_srl'];
+				$name = $_SESSION['notify_name'];
+				$type = $_SESSION['notify_type'];
+				unset($_SESSION['notify_type']);
+				unset($_SESSION['notify_title']);
+				unset($_SESSION['notify_srl']);
+				unset($_SESSION['notify_name']);
+				$new_document_notify = "<script type='text/javascript'>
+											socket.emit('sendToServer', {title :'".$title."' , document_srl : '".$srl."' , name : '".$name."' , type : '".$type."'});
+										</script>";
+				Context::addHtmlHeader($new_document_notify);
+			}else{
+				
+				if($_SESSION['notify_type']=='comment') unset($_SESSION['notify_name']);
+				unset($_SESSION['notify_modify']);
+				unset($_SESSION['notify_type']);
+				unset($_SESSION['notify_title']);
+				unset($_SESSION['notify_srl']);
+			}
+		}
+	}	
 
 }else if($called_position == 'after_module_proc'){
+	
 	if(Context::get('act') == 'procBoardInsertDocument'){
 		$document_srl = $this->get('document_srl');
-		//$oDocumentModel = &getModel('document');
 		$oDocument = Context::getRequestVars();
 		$title = $oDocument->title;
-		if($oDocument->document_srl && $oDocument->status == 'PUBLIC') $_SESSION['notify_type'] = 'modify';
-		else $_SESSION['notify_type'] = 'write';
-			
+		if($_SESSION['notify_modify'] != 'modify') $_SESSION['notify_type'] = 'write';
 		$_SESSION['notify_title'] = strip_tags($title);
 		$_SESSION['notify_srl'] = $document_srl;
 	}else if(Context::get('act') == 'procBoardInsertComment' && $addon_info->comment == 'Y'){
@@ -65,14 +74,15 @@ if($called_position == 'before_module_proc' && Context::get('module') != 'admin'
 		$comment_srl = $this->get('comment_srl');
 		$oComment = &getModel('comment')->getComment($comment_srl);
 		$commenter = $oComment->nick_name;
-			
+		
 		//set SESSION
 		$_SESSION['notify_type'] = 'comment';
 		$_SESSION['notify_title'] = strip_tags(cut_str($title,$addon_info->cut_title,'…'));
 		$_SESSION['notify_srl'] = $this->mid.'/'.$document_srl.'#comment_'.$comment_srl;
 		$_SESSION['notify_name'] = strip_tags($commenter);
-			
+		
 	}
 
 }
 ?>
+
