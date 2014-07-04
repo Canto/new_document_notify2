00ac1f0a2c897b40f598d85c34372a791b229992
canto87-mac <canto87@naver.com>
2014-07-04 15:57:36 +0900

    origin 설정 추가 (보안패치)

--- js/server.js
+++ js/server.js
@@ -1,5 +1,8 @@
 var io = require('socket.io').listen('3000');
 // open the socket connection
+io.set('origins', '//홈페이지주소:*');
+// ex) io.set('origins', '//naver.com:*');
+
 io.on('connection', function (socket) {
     //call back trigger to server
     socket.on('sendToServer',function(data){
